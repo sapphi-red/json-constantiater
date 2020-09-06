ba28ebbbe6cf490718b43d2dae4e991c1846d8a8
sapphi-red <green@sapphi.red>
2020-09-07 04:54:24 +0900

    Update: remove if from AppendByteWithEscape/GetEscapedLen

--- lib/string.go
+++ lib/string.go
@@ -15,12 +15,10 @@ func AppendByteWithEscape(dest []byte, src string) []byte {
 			dest = append(dest, '\\', 'r')
 		case '\t':
 			dest = append(dest, '\\', 't')
+		case byte(0x0), byte(0x1), byte(0x2), byte(0x3), byte(0x4), byte(0x5), byte(0x6), byte(0x7), byte(0x8), byte(0xb), byte(0xc), byte(0xe), byte(0xf), byte(0x10), byte(0x11), byte(0x12), byte(0x13), byte(0x14), byte(0x15), byte(0x16), byte(0x17), byte(0x18), byte(0x19), byte(0x1a), byte(0x1b), byte(0x1c), byte(0x1d), byte(0x1e), byte(0x1f):
+			dest = append(dest, 'u', '0', '0', hextable[c>>4], hextable[c&0x0f])
 		default:
-			if c < byte(0x20) {
-				dest = append(dest, 'u', '0', '0', hextable[c>>4], hextable[c&0x0f])
-			} else {
-				dest = append(dest, c)
-			}
+			dest = append(dest, c)
 		}
 	}
 	return dest
@@ -32,12 +30,10 @@ func GetEscapedLen(src string) uint64 {
 		switch c {
 		case '\\', '"', '\n', '\r', '\t':
 			l += 2
+		case byte(0x0), byte(0x1), byte(0x2), byte(0x3), byte(0x4), byte(0x5), byte(0x6), byte(0x7), byte(0x8), byte(0xb), byte(0xc), byte(0xe), byte(0xf), byte(0x10), byte(0x11), byte(0x12), byte(0x13), byte(0x14), byte(0x15), byte(0x16), byte(0x17), byte(0x18), byte(0x19), byte(0x1a), byte(0x1b), byte(0x1c), byte(0x1d), byte(0x1e), byte(0x1f):
+			l += 5
 		default:
-			if c < byte(0x20) {
-				l += 5
-			} else {
-				l++
-			}
+			l++
 		}
 	}
 	return l
