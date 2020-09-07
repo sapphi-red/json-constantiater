48c1b9f5abab967184191ba58878acb8e3cdf645
sapphi-red <green@sapphi.red>
2020-09-08 01:37:43 +0900

    Fix: escaping \r \n \t was broken

--- lib/string.go
+++ lib/string.go
@@ -4,11 +4,15 @@ const hextable = "0123456789abcdef"
 
 func AppendByteWithEscape(dest []byte, src string) []byte {
 	for _, c := range []byte(src) {
-		if c == '\\' || c == '"' {
+		if c == '"' || c == '\\' {
 			dest = append(dest, '\\', c)
 		} else if c < 0x20 {
-			if c == '\n' || c == '\r' || c == '\t' {
-				dest = append(dest, '\\', c)
+			if c == '\n' {
+				dest = append(dest, '\\', 'n')
+			} else if c == '\r' {
+				dest = append(dest, '\\', 'r')
+			} else if c == '\t' {
+				dest = append(dest, '\\', 't')
 			} else {
 				dest = append(dest, 'u', '0', '0', hextable[c>>4], hextable[c&0x0f])
 			}
@@ -22,7 +26,7 @@ func AppendByteWithEscape(dest []byte, src string) []byte {
 func GetEscapedLen(src string) uint64 {
 	var l uint64 = uint64(len(src))
 	for _, c := range []byte(src) {
-		if c == '\\' || c == '"' {
+		if c == '"' || c == '\\' {
 			l++
 		} else if c < 0x20 {
 			if c == '\n' || c == '\r' || c == '\t' {
