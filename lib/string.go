63a1ee1fc3afed7660f570b59f05580d67de655d
sapphi-red <green@sapphi.red>
2020-09-07 19:59:34 +0900

    Update: improve GetEscapedLen

--- lib/string.go
+++ lib/string.go
@@ -25,15 +25,13 @@ func AppendByteWithEscape(dest []byte, src string) []byte {
 }
 
 func GetEscapedLen(src string) uint64 {
-	var l uint64 = 0
+	var l uint64 = uint64(len(src))
 	for _, c := range []byte(src) {
 		switch c {
 		case '\\', '"', '\n', '\r', '\t':
-			l += 2
+			l ++
 		case byte(0x0), byte(0x1), byte(0x2), byte(0x3), byte(0x4), byte(0x5), byte(0x6), byte(0x7), byte(0x8), byte(0xb), byte(0xc), byte(0xe), byte(0xf), byte(0x10), byte(0x11), byte(0x12), byte(0x13), byte(0x14), byte(0x15), byte(0x16), byte(0x17), byte(0x18), byte(0x19), byte(0x1a), byte(0x1b), byte(0x1c), byte(0x1d), byte(0x1e), byte(0x1f):
-			l += 5
-		default:
-			l++
+			l += 4
 		}
 	}
 	return l
