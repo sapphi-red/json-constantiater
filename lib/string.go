ec3f99f1f60295da28e41b4ec187b0b5ff93e4d8
sapphi-red <green@sapphi.red>
2020-09-08 00:25:16 +0900

    Update: optimize condition AppendByteWithEscape/GetEscapedLen

--- lib/string.go
+++ lib/string.go
@@ -4,20 +4,15 @@ const hextable = "0123456789abcdef"
 
 func AppendByteWithEscape(dest []byte, src string) []byte {
 	for _, c := range []byte(src) {
-		switch c {
-		case '\\':
-			dest = append(dest, '\\', '\\')
-		case '"':
-			dest = append(dest, '\\', '"')
-		case '\n':
-			dest = append(dest, '\\', 'n')
-		case '\r':
-			dest = append(dest, '\\', 'r')
-		case '\t':
-			dest = append(dest, '\\', 't')
-		case byte(0x0), byte(0x1), byte(0x2), byte(0x3), byte(0x4), byte(0x5), byte(0x6), byte(0x7), byte(0x8), byte(0xb), byte(0xc), byte(0xe), byte(0xf), byte(0x10), byte(0x11), byte(0x12), byte(0x13), byte(0x14), byte(0x15), byte(0x16), byte(0x17), byte(0x18), byte(0x19), byte(0x1a), byte(0x1b), byte(0x1c), byte(0x1d), byte(0x1e), byte(0x1f):
-			dest = append(dest, 'u', '0', '0', hextable[c>>4], hextable[c&0x0f])
-		default:
+		if c == '\\' || c == '"' {
+			dest = append(dest, '\\', c)
+		} else if c < 0x20 {
+			if c == '\n' || c == '\r' || c == '\t' {
+				dest = append(dest, '\\', c)
+			} else {
+				dest = append(dest, 'u', '0', '0', hextable[c>>4], hextable[c&0x0f])
+			}
+		} else {
 			dest = append(dest, c)
 		}
 	}
@@ -27,11 +22,14 @@ func AppendByteWithEscape(dest []byte, src string) []byte {
 func GetEscapedLen(src string) uint64 {
 	var l uint64 = uint64(len(src))
 	for _, c := range []byte(src) {
-		switch c {
-		case '\\', '"', '\n', '\r', '\t':
-			l ++
-		case byte(0x0), byte(0x1), byte(0x2), byte(0x3), byte(0x4), byte(0x5), byte(0x6), byte(0x7), byte(0x8), byte(0xb), byte(0xc), byte(0xe), byte(0xf), byte(0x10), byte(0x11), byte(0x12), byte(0x13), byte(0x14), byte(0x15), byte(0x16), byte(0x17), byte(0x18), byte(0x19), byte(0x1a), byte(0x1b), byte(0x1c), byte(0x1d), byte(0x1e), byte(0x1f):
-			l += 4
+		if c == '\\' || c == '"' {
+			l++
+		} else if c < 0x20 {
+			if c == '\n' || c == '\r' || c == '\t' {
+				l++
+			} else {
+				l += 4
+			}
 		}
 	}
 	return l
